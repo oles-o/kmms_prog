a17ec1ea43c28097f6168f074c65fbc30240c08c
Olesya A <arilisikaaa@gmail.com>
2025-10-17 17:38:13 +0300

    Refactoring practice from lesson

---
+++ 251017_refactoring/after_refactoring_3.cpp
@@ -0,0 +1,28 @@
+﻿#include <cmath>
+#include <iostream>
+
+int main() {
+	const int arr_size = 20;
+    int arr[arr_size];
+	
+	std::cout << "Сгенерированный массив: ";
+    for (int i = 0; i < arr_size; i++) {
+        arr[i] = rand() % 10;
+        std::cout << arr[i] << " ";
+    }
+	std::cout << std::endl;
+	
+	
+    for (int i = 0; i < arr_size / 2; i++) {
+        int element = arr[i];
+        arr[i] = arr[arr_size - 1 - i];
+        arr[arr_size - 1 - i] = element;
+    }
+	
+	std::cout << "Перевернутый массив: ";
+    for (int i = 0; i < arr_size; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
---
+++ 251017_refactoring/build_run.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 1251 > log
+del log
+
+set MAIN=after_refactoring_3.cpp
+set EXE=example.exe
+
+set CHARSET="-finput-charset=utf-8 -fexec-charset=windows-1251"
+
+if exist %EXE% del %EXE%
+
+g++ "%CHARSET%" %MAIN% -o %EXE%
+
+%EXE%
